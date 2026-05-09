001d3ef66b67cfbc5e37ac022580118020c48e5b
JasonQorbin <Jason.Pieterse@Outlook.com>
2026-05-09 16:52:08 +0200

    Separate OS build from App build
    Separated the Ubuntu upgrade process into it's own image to avoid having
    to waste time downloading the updates again and again.

--- mynextcloud-build/install.sh
+++ mynextcloud-build/install.sh
@@ -23,49 +23,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 DEFAULT_COLOUR='\033[0m'
 
-
-echo -e "${GREEN}Updating system${DEFAULT_COLOUR}"
-apt-get update -y
-apt-get install rsync -y
-apt-get install -y software-properties-common
-add-apt-repository ppa:ondrej/php -y
-add-apt-repository ppa:ondrej/apache2 -y
-apt-get install -y nano wget bzip2
-apt-get upgrade -y
-
-# Install tzdata and set timezone so that the installation doesn't get interrupted.
-# If the TIME_ZONE environment variable has not been set then use UCT as the default.
-if [[ -z "${TIME_ZONE}" ]]; then
-    TIME_ZONE="UCT"
-fi
-
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends tzdata
-
-timedatectl set-timezone ${TIME_ZONE}
-
-# Install PHP
-echo -e "${GREEN}Installing PHP and required modules${DEFAULT_COLOUR}"
-apt-get install -y php8.2 php8.2-xml php8.2-curl php8.2-gd php-json php8.2-mbstring php8.2-zip php8.2-mysql
-
-echo -e "${GREEN}Installing recommended PHP modules${DEFAULT_COLOUR}"
-apt-get install -y php8.2-bz2 php8.2-intl php8.2-smbclient php8.2-bcmath php8.2-gmp php8.2-redis php8.2-imagick
-
-# Change the memory limit in the configuration file per Nextcloud recommendations
-sed -i 's/.*memory_limit.*/memory_limit = 512M/' /etc/php/8.2/apache2/php.ini
-# Disable PHP output buffering per nextcloud recommendations
-sed -i 's/.*output_buffering.*/output_buffering = off/' /etc/php/8.2/apache2/php.ini
-
-# Install ffmpeg to allow for meadia playback. This is a large package.
-echo -e "${GREEN}Installing ffmpeg${DEFAULT_COLOUR}"
-apt-get install -y ffmpeg
-
-# Install libreoffice to allow opening of office-type files
-echo -e "${GREEN}Installing LibreOffice${DEFAULT_COLOUR}"
-apt-get install -y libreoffice
-
-
-echo -e "${GREEN}Installing Apache2${DEFAULT_COLOUR}"
-apt-get install -y apache2
+#INSERT OS setup here
 
 # Bake in a default copy of Nextcloud
 embeddedInstallLocation=/usr/src/nextcloud
