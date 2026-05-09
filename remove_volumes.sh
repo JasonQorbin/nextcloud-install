be0758984bc8a3e065de45b13b3697160e7d14fc
JasonQorbin <Jason.Pieterse@Outlook.com>
2026-05-09 16:31:37 +0200

    Add script for removing named volumes
    The fastest way to test a fresh install is to remove the named volumes
    created by the docker compose process. This is a convenience script to
    save time.

---
+++ remove_volumes.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker volume rm nextcloud-upgrade_nextcloud_test_apps
+docker volume rm nextcloud-upgrade_nextcloud_test_config
+docker volume rm nextcloud-upgrade_nextcloud_test_themes
+docker volume rm nextcloud-upgrade_nextcloud_test_data
+docker volume rm nextcloud-upgrade_nextcloud_test_install
