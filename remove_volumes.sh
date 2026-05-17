88350289798f9b365afe54fc61672fd0a04f5dad
JasonQorbin <Jason.Pieterse@Outlook.com>
2026-05-17 18:39:26 +0200

    Change database storage location
    Moved the database to a named volume and changed the mariadb container
    to use the standard image environment variables so that the container
    will make the database, the user and assign the privileges on it's own
    without needing a setup script.

--- remove_volumes.sh
+++ remove_volumes.sh
@@ -4,3 +4,4 @@ docker volume rm nextcloud-upgrade_nextcloud_test_config
 docker volume rm nextcloud-upgrade_nextcloud_test_themes
 docker volume rm nextcloud-upgrade_nextcloud_test_data
 docker volume rm nextcloud-upgrade_nextcloud_test_install
+docker volume rm nextcloud-upgrade_database_volume
