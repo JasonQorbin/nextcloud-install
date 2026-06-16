26da53f3f1eae9c3c083d580b7e03df3d823a9c6
JasonQorbin <Jason.Pieterse@Outlook.com>
2026-06-16 15:08:04 +0200

    Bug fix: Typo in volume removal script
    Forgot to swap one of the underscores with a hyphen in the volume
    name.

--- remove_volumes.sh
+++ remove_volumes.sh
@@ -4,7 +4,7 @@ directory_name=$(basename "$PWD")
 
 echo "Removing named volumes used by the application:"
 docker volume rm "${directory_name}_nextcloud-config"
-docker volume rm "${directory_name}_nextcloud-user_data"
+docker volume rm "${directory_name}_nextcloud-user-data"
 docker volume rm "${directory_name}_nextcloud-themes"
 docker volume rm "${directory_name}_nextcloud-install"
 docker volume rm "${directory_name}_maria-database-volume"
